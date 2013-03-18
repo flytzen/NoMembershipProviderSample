58fa32c2357c71413e2c045d7748c469da1aa7c4
Frans Lytzen <flytzen@neworbit.co.uk>
2013-03-18 21:23:14 +0000

    And with roles as well

--- NoMembershipProviderSample/Global.asax.cs
+++ NoMembershipProviderSample/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using NoMembershipProviderSample.Models;
 
 namespace NoMembershipProviderSample
 {
@@ -19,6 +22,29 @@ namespace NoMembershipProviderSample
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            var ctx = HttpContext.Current;
+            if (ctx.Request.IsAuthenticated)
+            {
+                string[] roles = LookupRolesForUser(ctx.User.Identity.Name);
+                var newUser = new GenericPrincipal(ctx.User.Identity, roles);
+                ctx.User = Thread.CurrentPrincipal = newUser;
+            }
+        }
+
+        private string[] LookupRolesForUser(string name)
+        {
+            var repo = new AccountRepository(); // In the real world, you would probably use service locator pattern and call DependencyResolver here
+            var user = repo.FindByName(name);
+            if (user != null)
+            {
+                return user.Roles;
+            }
+
+            return new string[0];  // Alternatively throw an exception
         }
     }
 }
